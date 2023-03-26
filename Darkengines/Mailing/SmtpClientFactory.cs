bb4b40fc36419fd25e2f2d19581ec28f96a4e1cb
Florent Tollin de Rivarol <florent@comeet.in>
2023-03-26 14:31:57 +0200

    introducing mailing

---
+++ Darkengines/Mailing/SmtpClientFactory.cs
@@ -0,0 +1,27 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Darkengines.Mailing {
+	public class SmtpClientFactory {
+		protected IOptions<SmtpOptions> Options { get; }
+		public SmtpClientFactory(IOptions<SmtpOptions> options) {
+			Options = options;
+		}
+		public async Task<SmtpClient> CreateSmtpClient() {
+			var smtpClient = new SmtpClient();
+			await smtpClient.ConnectAsync(Options.Value.Host, Options.Value.Port, Options.Value.UseSsl);
+			if (Options.Value.Credentials != null) {
+				var credentials = new NetworkCredential(Options.Value.Credentials.UserName, Options.Value.Credentials.Password);
+				await smtpClient.AuthenticateAsync(credentials);
+			}
+			return smtpClient;
+		}
+	}
+}
